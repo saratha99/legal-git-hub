a10796793f0df0677250d2b42735ca675a77caeb
saratha99 <46121206+saratha99@users.noreply.github.com>
2019-01-27 17:22:10 +0530

    set1pro1.c

---
+++ set1pro1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include<conio.h>
+void main()
+{
+    int n;
+    clrscr();
+    scanf("%d",&n);
+    if(n>0)
+    printf("positive");
+    else
+    printf("negative");
+    getch();
+}
